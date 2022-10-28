e8ab1426c8a8f309e4c59cc907c3a0828cfa268e
BadCpp <92181681+BadCpp@users.noreply.github.com>
2022-10-27 20:51:37 -0400

    Com2
    Com2

---
+++ pkg/src/pkg/main.py
@@ -0,0 +1,276 @@
+import time
+import gym
+import numpy as np
+import concurrent.futures
+import os
+import sys
+
+# Get ./src/ folder & add it to path
+current_dir = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(current_dir)
+
+# import your drivers here
+from drivers import *
+
+# choose your drivers here (1-4)
+drivers = [GapFollower()]
+
+# choose your racetrack here (SOCHI, SOCHI_OBS)
+RACETRACK = 'SOCHI'
+
+
+def _pack_odom(obs, i):
+    keys = {
+        'poses_x': 'pose_x',
+        'poses_y': 'pose_y',
+        'poses_theta': 'pose_theta',
+        'linear_vels_x': 'linear_vel_x',
+        'linear_vels_y': 'linear_vel_y',
+        'ang_vels_z': 'angular_vel_z',
+    }
+    return {single: obs[multi][i] for multi, single in keys.items()}
+
+
+class GymRunner(object):
+
+    def __init__(self, racetrack, drivers):
+        self.racetrack = racetrack
+        self.drivers = drivers
+
+    def run(self):
+        # load map
+        env = gym.make('f110_gym:f110-v0',
+                       map="{}/maps/{}".format(current_dir, RACETRACK),
+                       map_ext=".png", num_agents=len(drivers))
+
+        # specify starting positions of each agent
+        driver_count = len(drivers)
+        if driver_count == 1:
+            poses = np.array([[0.8007017, -0.2753365, 4.1421595]])
+        elif driver_count == 2:
+            poses = np.array([
+                [0.8007017, -0.2753365, 4.1421595],
+                [0.8162458, 1.1614572, 4.1446321],
+            ])
+        else:
+            raise ValueError("Max 2 drivers are allowed")
+
+        obs, step_reward, done, info = env.reset(poses=poses)
+        env.render()
+
+        laptime = 0.0
+        start = time.time()
+
+        while not done:
+            actions = []
+            futures = []
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                odom_0, odom_1 = _pack_odom(obs, 0), None
+                if len(drivers) > 1:
+                    odom_1 = _pack_odom(obs, 1)
+
+                for i, driver in enumerate(drivers):
+                    if i == 0:
+                        ego_odom, opp_odom = odom_0, odom_1
+                    else:
+                        ego_odom, opp_odom = odom_1, odom_0
+                    scan = obs['scans'][i]
+                    if hasattr(driver, 'process_observation'):
+                        futures.append(executor.submit(driver.process_observation, ranges=scan, ego_odom=ego_odom))
+                    elif hasattr(driver, 'process_lidar'):
+                        futures.append(executor.submit(driver.process_lidar, scan))
+
+            for future in futures:
+                speed, steer = future.result()
+                actions.append([steer, speed])
+            actions = np.array(actions)
+            obs, step_reward, done, info = env.step(actions)
+            #print(obs,"\n",step_reward,"\n",done,"\n",all(obs["collisions"]))
+            laptime += step_reward
+            if (any(obs["lap_counts"])):
+                 return
+            env.render(mode='human')
+
+        print('Sim elapsed time:', laptime, 'Real elapsed time:', time.time() - start)
+
+
+class BlindRunner(object):
+
+    def __init__(self, racetrack, drivers):
+        self.racetrack = racetrack
+        self.drivers = drivers
+
+    def run(self):
+        # load map
+        env = gym.make('f110_gym:f110-v0',
+                       map="{}/maps/{}".format(current_dir, RACETRACK),
+                       map_ext=".png", num_agents=len(drivers))
+
+        # specify starting positions of each agent
+        driver_count = len(drivers)
+        if driver_count == 1:
+            poses = np.array([[0.8007017, -0.2753365, 4.1421595]])
+        elif driver_count == 2:
+            poses = np.array([
+                [0.8007017, -0.2753365, 4.1421595],
+                [0.8162458, 1.1614572, 4.1446321],
+            ])
+        else:
+            raise ValueError("Max 2 drivers are allowed")
+
+        obs, step_reward, done, info = env.reset(poses=poses)
+        #env.render()
+
+        laptime = 0.0
+        start = time.time()
+
+        while not done:
+            actions = []
+            futures = []
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                odom_0, odom_1 = _pack_odom(obs, 0), None
+                if len(drivers) > 1:
+                    odom_1 = _pack_odom(obs, 1)
+
+                for i, driver in enumerate(drivers):
+                    if i == 0:
+                        ego_odom, opp_odom = odom_0, odom_1
+                    else:
+                        ego_odom, opp_odom = odom_1, odom_0
+                    scan = obs['scans'][i]
+                    if hasattr(driver, 'process_observation'):
+                        futures.append(executor.submit(driver.process_observation, ranges=scan, ego_odom=ego_odom))
+                    elif hasattr(driver, 'process_lidar'):
+                        futures.append(executor.submit(driver.process_lidar, scan))
+
+            for future in futures:
+                speed, steer = future.result()
+                actions.append([steer, speed])
+            actions = np.array(actions)
+            obs, step_reward, done, info = env.step(actions) 
+            
+            #print(obs["collisions"])
+            if (all(obs["collisions"])):
+                return None
+            if (any(obs["lap_counts"])):
+                return laptime
+            laptime += step_reward
+            #env.render(mode='human')
+
+        #print('Sim elapsed time:', laptime, 'Real elapsed time:', time.time() - start)
+
+
+#takes in string for class that is to be tested.
+#Class must be named "TestRunner"
+#returns a time (None if failed)
+def testInput(input, args = None):
+    if (args):
+        pass #do more stuff here i guess
+    input = "import numpy as np\n"+input
+    try:
+        with open("testRuns.py","w+") as book:
+            book.write(input)
+
+        import testRuns
+
+        runner = BlindRunner(RACETRACK, [testRuns.TestRunner()])
+        return runner.run()
+    except:
+        print("Your code failed to compile. \nL.")
+        print("We generated:\n"+input.__repr__())
+        return None
+    
+ans = """
+class TestRunner:
+    BUBBLE_RADIUS = 160
+    PREPROCESS_CONV_SIZE = 3
+    BEST_POINT_CONV_SIZE = 80
+    MAX_LIDAR_DIST = 3000000
+    STRAIGHTS_SPEED = 8.0
+    CORNERS_SPEED = 5.0
+    STRAIGHTS_STEERING_ANGLE = np.pi / 18  # 10 degrees
+
+    def __init__(self):
+        # used when calculating the angles of the LiDAR data
+        self.radians_per_elem = None
+
+    def preprocess_lidar(self, ranges):
+        
+        self.radians_per_elem = (2 * np.pi) / len(ranges)
+        # we won't use the LiDAR data from directly behind us
+        proc_ranges = np.array(ranges[135:-135])
+        # sets each value to the mean over a given window
+        proc_ranges = np.convolve(proc_ranges, np.ones(self.PREPROCESS_CONV_SIZE), 'same') / self.PREPROCESS_CONV_SIZE
+        proc_ranges = np.clip(proc_ranges, 0, self.MAX_LIDAR_DIST)
+        return proc_ranges
+
+    def find_max_gap(self, free_space_ranges):
+        # Return the start index & end index of the max gap in free_space_ranges
+        #    free_space_ranges: list of LiDAR data which contains a 'bubble' of zeros
+        
+        # mask the bubble
+        masked = np.ma.masked_where(free_space_ranges == 0, free_space_ranges)
+        # get a slice for each contigous sequence of non-bubble data
+        slices = np.ma.notmasked_contiguous(masked)
+        max_len = slices[0].stop - slices[0].start
+        chosen_slice = slices[0]
+        # I think we will only ever have a maximum of 2 slices but will handle an
+        # indefinitely sized list for portablility
+        for sl in slices[1:]:
+            sl_len = sl.stop - sl.start
+            if sl_len > max_len:
+                max_len = sl_len
+                chosen_slice = sl
+        return chosen_slice.start, chosen_slice.stop
+
+    def find_best_point(self, start_i, end_i, ranges):
+        #Start_i & end_i are start and end indices of max-gap range, respectively
+        #Return index of best point in ranges
+        #Naive: Choose the furthest point within ranges and go there
+        
+        # do a sliding window average over the data in the max gap, this will
+        # help the car to avoid hitting corners
+        averaged_max_gap = np.convolve(ranges[start_i:end_i], np.ones(self.BEST_POINT_CONV_SIZE),
+                                       'same') / self.BEST_POINT_CONV_SIZE
+        return averaged_max_gap.argmax() + start_i
+
+    def get_angle(self, range_index, range_len):
+        # Get the angle of a particular element in the LiDAR data and transform it into an appropriate steering angle
+        
+        lidar_angle = (range_index - (range_len / 2)) * self.radians_per_elem
+        steering_angle = lidar_angle / 2
+        return steering_angle
+
+    def process_lidar(self, ranges):
+        # Process each LiDAR scan as per the Follow Gap algorithm & publish an AckermannDriveStamped Message
+        #
+        proc_ranges = self.preprocess_lidar(ranges)
+        # Find closest point to LiDAR
+        closest = proc_ranges.argmin()
+
+        # Eliminate all points inside 'bubble' (set them to zero)
+        min_index = closest - self.BUBBLE_RADIUS
+        max_index = closest + self.BUBBLE_RADIUS
+        if min_index < 0: min_index = 0
+        if max_index >= len(proc_ranges): max_index = len(proc_ranges) - 1
+        proc_ranges[min_index:max_index] = 0
+
+        # Find max length gap
+        gap_start, gap_end = self.find_max_gap(proc_ranges)
+
+        # Find the best point in the gap
+        best = self.find_best_point(gap_start, gap_end, proc_ranges)
+
+        # Publish Drive message
+        steering_angle = self.get_angle(best, len(proc_ranges))
+        if abs(steering_angle) > self.STRAIGHTS_STEERING_ANGLE:
+            speed = self.CORNERS_SPEED
+        else:
+            speed = self.STRAIGHTS_SPEED
+        return speed, steering_angle
+"""
+
+if __name__ == '__main__':
+    print(testInput(ans))
+    #runner = GymRunner(RACETRACK, drivers)
+    #runner.run()
