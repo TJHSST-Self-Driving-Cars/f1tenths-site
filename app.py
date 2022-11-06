d789eca88e00e42c046699a06fd12f1e1509e889
FluffyCube9343 <learningninja007@gmail.com>
2022-11-06 09:30:14 -0500

    (maybe unstable) manual merge from master

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import multiprocessing
 import time
 import pickle
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 import traceback
 from flask_assets import Bundle, Environment
 import simulator.pkg.src.pkg.main as simCode
@@ -15,27 +15,25 @@ css = Bundle("src/main.css", output="dist/main.css")
 assets.register("css", css)
 css.build()
 
-
-
-def f(ads):
-    return 10
-
 def simulate(code):
-    process = multiprocessing.Process(target=f,args=(code,))
+    outL = multiprocessing.Value('d', 0.0)
+    process = multiprocessing.Process(target=simCode.testInputDaemon,args=(code,outL))
     process.daemon = True
     process.start()
-    process.join(5)
+    
+    process.join(50)
+    res = outL.value
     if process.is_alive():
         print("Test is hanging!")
         process.terminate()
         print("Terminated!")
-        return process.get()
-    return process.get()
+        return -1
+    return round(res,3)
 
 class Item:
     def __init__(self,name,time):
         self.name=name
-        self.time=int(time)
+        self.time=float(time)
         self.rank=0
     def __repr__(self):
         return self.name+" "+str(self.time)
@@ -52,7 +50,6 @@ def readPlayers():
     try:
         file = open('members.pickle', 'rb')
         o = pickle.load(file)
-        
         return o
     except FileNotFoundError:
         pass
@@ -83,9 +80,9 @@ def appendPlayer(item):
 
 @app.route('/leaderboard', methods =["GET", "POST"])
 def leaderboard():
+    print("table",readPlayers())
+    return render_template("leaderboard.html", login_status="", table = readPlayers())
 
-    if request.method == "POST":
-        return render_template("leaderboard.html", isLogged="", table = readPlayers())
 
 @app.route('/leaderboard2', methods =["GET", "POST"])
 def leaderboard2():
@@ -99,18 +96,20 @@ def homepage():
     return render_template("index.html", login_status="")
 
 
+
 @app.route('/yay', methods =["GET", "POST"])
 def codesubmit():
     if request.method == "POST":
-        code = request.form.get("code_")
-        try:
-            result = simCode.testCode(code)
-            return render_template("yay.html", run_time=result)
+        uid = request.cookies.get('userID')
 
-        except Exception as e:
-            
-            tb = traceback.format_exc()
-            return render_template("error.html", error_=tb)
+        code = request.form.get("code_")
+
+        result = simulate(simCode.ans)
+        #print(result,":D")
+        if (result != -1):
+            appendPlayer(Item(uid, result)) #log result of the code run
+        return render_template("leaderboard.html", run_time=result,  table = readPlayers())
+    return render_template("leaderboard.html", table = readPlayers())
 
 @app.route('/base', methods =["GET", "POST"])
 def homeFromLeaderboard():
@@ -124,18 +123,21 @@ def login():
         first_name = request.form.get("username")
         
         last_name = request.form.get("pw")
+
+        
+
         if(first_name=="user"):
-            
-            result = f("Ad") #simCode.testCode()
-            if (result > 0): #if successful
-                appendPlayer(Item(first_name,result))
-            return render_template("yay.html", run_time=str(result))
+            return render_template("leaderboard.html", run_time=1)
         elif(first_name=="test"):
-            return render_template("home.html")
-        # getting input with name = fname in HTML form
-        # getting input with name = lname in HTML form
+            resp = make_response(render_template("home.html"))
+            resp.set_cookie('userID', first_name)
+            return resp
         else:
             return render_template("index.html", login_status="Wrong username/password")
+        
+        # getting input with name = fname in HTML form
+        # getting input with name = lname in HTML form
+    
 @app.route('/codesubmit', methods =["GET", "POST"])
 def toCode():
     
