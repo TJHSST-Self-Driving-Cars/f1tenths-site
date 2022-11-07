e834dda09f8b535c5b521bbbbf078f0be2a47026
BadCpp <92181681+BadCpp@users.noreply.github.com>
2022-11-07 15:38:06 -0500

    app.py update
    :D

--- app.py
+++ app.py
@@ -2,12 +2,39 @@
 import multiprocessing
 import time
 import pickle
-from flask import Flask, render_template, request, make_response
+from flask import Flask, render_template, request, make_response, redirect, url_for
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user,
+)
+
+from oauthlib.oauth2 import WebApplicationClient
+import requests
 
 from flask_assets import Bundle, Environment
 import simulator.pkg.src.pkg.main as simCode
 
+import json
+import os
+import sqlite3
+
+from db import init_db_command
+from user import User
+
+
+# Configuration
+GOOGLE_CLIENT_ID = "886442690079-alqevh0t0f5vgamvkvnogsm9m33vio1q.apps.googleusercontent.com" #os.environ.get("GOOGLE_CLIENT_ID", None)
+GOOGLE_CLIENT_SECRET = "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" #os.environ.get("GOOGLE_CLIENT_SECRET", None)
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
+
+# Flask app setup
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
 
 assets = Environment(app)
 css = Bundle("src/main.css", output="dist/main.css")
@@ -15,7 +42,32 @@ css = Bundle("src/main.css", output="dist/main.css")
 assets.register("css", css)
 css.build()
 
+# User session management setup
+# https://flask-login.readthedocs.io/en/latest
+login_manager = LoginManager()
+login_manager.init_app(app)
 
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return "You must be logged in to access this content.", 403
+
+
+# Naive database setup
+try:
+    init_db_command()
+except sqlite3.OperationalError:
+    # Assume it's already been created
+    pass
+
+# OAuth2 client setup
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
+
+
+# Flask-Login helper to retrieve a user from our db
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
 
 def simulate(code):
@@ -43,11 +95,6 @@ class Item:
     def __lt__(self,other):
         return self.time < other.time
 
-
-items = [         
-         Item("Vrishak","6"),
-         Item('Krishnan','9'),
-         Item('Me','11')]
 
 def readPlayers():
     try:
@@ -82,52 +129,122 @@ def appendPlayer(item):
 
 @app.route('/leaderboard', methods =["GET", "POST"])
 def leaderboard():
-    print("table",readPlayers())
-    return render_template("leaderboard.html", login_status="", table = readPlayers())
+    #print("table",readPlayers())
+    if current_user.is_authenticated:
+        return render_template("leaderboard-logout.html", login_status="", table = readPlayers())
+    else:
+        return render_template("leaderboard-login.html", login_status="", table = readPlayers())
 
-
-@app.route("/")
-def homepage():
-    return render_template("index.html", login_status="")
-
-
+#https://realpython.com/flask-google-login/
 @app.route('/codesubmit', methods =["GET", "POST"])
 def codesubmit():
-    if request.method == "POST":
-        uid = request.cookies.get('userID')
 
-        code = request.form.get("code_")
+    code = request.form.get("code_")
 
-        result = simulate(simCode.ans)
-        #print(result,":D")
-        if (result != -1):
-            appendPlayer(Item(uid, result)) #log result of the code run
-        return render_template("leaderboard.html", run_time=result,  table = readPlayers())
-    return render_template("leaderboard.html", table = readPlayers())
+    result = simulate(simCode.ans)
+    #print(result,":D")
+    if (result != -1):
+        appendPlayer(Item(current_user.name, result)) #log result of the code run
+    return render_template("leaderboard-logout.html", run_time=result,  table = readPlayers())
 
-@app.route('/index', methods =["GET", "POST"])
+@app.route("/")
+def index():
+    if current_user.is_authenticated:
+        return render_template("codesubmit.html")
+    else: 
+        return render_template("leaderboard-login.html",table=readPlayers())
+
+
+@app.route("/login")
 def login():
-    if request.method == "POST":
-        
-        first_name = request.form.get("username")
-        
-        last_name = request.form.get("pw")
+    # Find out what URL to hit for Google login
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
 
-        
+    # Use library to construct the request for login and provide
+    # scopes that let you retrieve user's profile from Google
+    #print(authorization_endpoint)
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+    )
+    #print(request_uri)
+    return redirect(request_uri)
 
-        if(first_name=="user"):
-            return render_template("leaderboard.html", run_time=1)
-        elif(first_name=="test"):
-            resp = make_response(render_template("codesubmit.html"))
-            resp.set_cookie('userID', first_name)
-            return resp
-        else:
-            return render_template("index.html", login_status="Wrong username/password")
-        
-        # getting input with name = fname in HTML form
-        # getting input with name = lname in HTML form
-        
+
+@app.route("/login/callback")
+def callback():
+    # Get authorization code Google sent back to you
+    code = request.args.get("code")
+
+    # Find out what URL to hit to get tokens that allow you to ask for
+    # things on behalf of a user
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+
+    # Prepare and send request to get tokens! Yay tokens!
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=request.url,
+        redirect_url=request.base_url,
+        code=code,
+    )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+    )
+
+    # Parse the tokens!
+    client.parse_request_body_response(json.dumps(token_response.json()))
+
+    # Now that we have tokens (yay) let's find and hit URL
+    # from Google that gives you user's profile information,
+    # including their Google Profile Image and Email
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+
+    # We want to make sure their email is verified.
+    # The user authenticated with Google, authorized our
+    # app, and now we've verified their email through Google!
+    if userinfo_response.json().get("email_verified"):
+        unique_id = userinfo_response.json()["sub"]
+        users_email = userinfo_response.json()["email"]
+        picture = userinfo_response.json()["picture"]
+        users_name = userinfo_response.json()["given_name"]
+    else:
+        return "User email not available or not verified by Google.", 400
+
+    # Create a user in our db with the information provided
+    # by Google
+    user = User(
+        id_=unique_id, name=users_name, email=users_email, profile_pic=picture
+    )
+
+    # Doesn't exist? Add to database
+    if not User.get(unique_id):
+        User.create(unique_id, users_name, users_email, picture)
+
+    # Begin user session by logging the user in
+    login_user(user)
+
+    # Send user back to homepage
+    return redirect(url_for("index"))
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
+
+
+def get_google_provider_cfg():
+    return requests.get(GOOGLE_DISCOVERY_URL).json()
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(ssl_context="adhoc", debug=True)
